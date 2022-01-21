5ef38c9c3310274b17414a2a44ecd9744faf7105
IhsanGan <mi598342@gmail.com>
2022-01-21 12:31:43 +0800

    readme

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Image-Uploader
 PHP image uploader &amp; auto embed for discord with Apache web server.
 ## Example
-<p align="center"><img src="https://im.isan.eu.org/up/xqjJtV.png"></p>
+<p align="center"><img src="https://im.isan.eu.org/up/cZ3PwK.png"></p>
 
 Make sure you've make a folder named "**up**" before uploading image!
--- json.php
+++ json.php
@@ -1,3 +1,1 @@
-<?php
-header("Content-Type: application/json");?>
-{"type":"link","version":"1.0","title":"view original","author_name":"<?php echo$_SERVER["QUERY_STRING"];?>"}
+<?php header("Content-Type: application/json");?>{"type":"link","version":"1.0","title":"view original","author_name":"<?php echo$_SERVER["QUERY_STRING"];?>"}
