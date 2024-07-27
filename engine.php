26b06cf93ec6931b421cda840b436ea18ae4324a
ihsangan <me@isan.eu.org>
2024-07-27 18:07:59 +0800

    update

--- engine.php
+++ engine.php
@@ -4,10 +4,30 @@ $query = $_GET["img"];
 $img = "up/$query";
 $link = "https://$host/$img";
 $url = "https://$host/$query";
-$prev = "https://i0.wp.com/$host/$img?w=1200&h=1200&quality=70";
+$cdn = "https://img.isan.eu.org/$host/$img";
+$disp = "$cdn?format=base";
+$prev = "$cdn?w=1200&h=1200&quality=70";
+$webp = "$cdn?format=webp";
 if (file_exists($img)) {
-    $time = filectime($img);
+    $time = date("D, d M Y H:i:s", filemtime($img));
+    $size = formatBytes(filesize($img));
+    list($width, $height) = getimagesize($img);
+    $wxh = "{$width}x{$height}";
+    $color = GenerateRandomColor();
 } else {
   header("HTTP/1.1 404 Not Found"); die();
+}
+function formatBytes($size, $precision = 2) {
+  $base = log($size, 1024);
+  $suffixes = array('B', 'KB', 'MB', 'GB', 'TB');
+  return round(pow(1024, $base - floor($base)), $precision) . $suffixes[floor($base)];
+}
+function GenerateRandomColor() {
+  $color = '#';
+  $colorHexLighter = array("0","A","F" );
+  for($x=0; $x < 6; $x++):
+  $color .= $colorHexLighter[array_rand($colorHexLighter, 1)]  ;
+  endfor;
+  return substr($color, 0, 7);
 }
 ?>
